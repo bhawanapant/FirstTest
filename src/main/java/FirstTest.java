22b06ba0b813b294dc98a988154d1615c0b50591
Bhawana Pant <Bhawana Pant>
2016-08-22 20:59:01 +0100

    First commit

---
+++ src/main/java/FirstTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+    public static final String CHROME_WEB_DRIVER_KEY = "webdriver.chrome.driver";
+    public static final String CHROME_WEB_DRIVER_EXE_PATH = "D:\\Software\\chromedriver\\chromedriver.exe";
+    public static final String BASE_URL = "https://google.co.uk";
+
+    public static void main(String[] args) {
+        System.setProperty(CHROME_WEB_DRIVER_KEY, CHROME_WEB_DRIVER_EXE_PATH);
+        WebDriver driver = new ChromeDriver();
+        String expectedTitle = "Google";
+        String actualTitle;
+
+        driver.get(BASE_URL);
+        actualTitle = driver.getTitle();
+
+
+        if (actualTitle.contentEquals(expectedTitle))
+            System.out.println("Test Passed");
+        else
+            System.out.println("Test Failed");
+
+        driver.close();
+    }
+}
